1acc15e84f451af5e1169fa759de6745bb259477
Gokul <gokul.email@example.com>
2024-12-02 14:01:32 +0530

    changes

---
+++ src/app/page.tsx
@@ -0,0 +1,178 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Card, CardHeader, CardContent } from '@/components/ui/card'
+import { Badge } from '@/components/ui/badge'
+import { Section } from '@/components/ui/section'
+import { ButtonLink } from '@/components/button-link'
+import { ProjectCard } from '@/components/project-card'
+import { CommandMenu } from '@/components/command-menu'
+import { GlobeIcon } from 'lucide-react'
+import { data } from '@/constants'
+
+export default function Page() {
+	return (
+		<main className='container relative mx-auto scroll-my-12 overflow-auto p-4 print:p-12 md:p-16'>
+			<section className='mx-auto w-full max-w-2xl space-y-8 bg-white print:space-y-6'>
+				<div className='flex items-center justify-between'>
+					<div className='flex-1 space-y-1.5'>
+						<h1 className='text-2xl font-bold'>{data.name}</h1>
+						<p className='max-w-md text-pretty font-mono text-sm text-muted-foreground'>
+							{data.about}
+						</p>
+						<p className='max-w-md items-center text-pretty font-mono text-xs text-muted-foreground'>
+							<a
+								className='inline-flex gap-x-1.5 align-baseline leading-none hover:underline'
+								href={data.locationLink}
+								target='_blank'
+								rel='noreferrer'
+							>
+								<GlobeIcon className='size-3' />
+								{data.location}
+							</a>
+						</p>
+
+						<ButtonLink data={data} />
+					</div>
+
+					<a href='https://github.com/Gokulraj-max' target='_blank' rel='noreferrer'>
+						<Avatar className='size-28 border'>
+							<AvatarImage alt={data.name} src={data.avatar} />
+							<AvatarFallback className='font-mono font-bold'>{data.initials}</AvatarFallback>
+						</Avatar>
+					</a>
+				</div>
+
+				<Section>
+					<h2 className='text-xl font-bold'>About</h2>
+					<p className='text-pretty font-mono text-sm text-muted-foreground'>{data.summary}</p>
+				</Section>
+
+				
+				<Section>
+					<h2 className='text-xl font-bold'>Education</h2>
+					{data.education.map((education) => (
+						<Card key={education.school}>
+							<CardHeader>
+								<h3 className='font-semibold leading-none text-base'>{education.school}</h3>
+							</CardHeader>
+							<CardContent className='mt-2 mb-4 text-xs'>
+								<div className='flex flex-col md:flex-row md:items-center justify-between gap-x-2 text-sm'>
+									<h4 className='font-mono text-primary'>{education.degree}</h4>
+									<time className='tabular-nums text-neutral-500'>
+										{education.start} - {education.end}
+									</time>
+								</div>
+								<ul className='mt-2'>
+									{education.description.map((item, index) => (
+										<li key={index} className='mb-1'>
+											• {item}
+										</li>
+									))}
+								</ul>
+							</CardContent>
+						</Card>
+					))}
+				</Section>
+
+				<Section>
+					<h2 className='text-xl font-bold'>Certification Course</h2>
+					{data.work.map((work) => (
+						<Card key={work.company}>
+							<CardHeader>
+								<h3 className='text-base inline-flex items-center gap-x-1 font-semibold leading-none'>
+									{work.link ? (
+										<a
+											className='hover:underline'
+											href={work.link}
+											target='_blank'
+											rel='noreferrer'
+										>
+											{work.company}
+										</a>
+									) : (
+										<span>{work.company}</span>
+									)}
+
+									<span className='inline-flex gap-x-1'>
+										{work.badges.map((badge) => (
+											<Badge variant='secondary' className='align-middle text-xs' key={badge}>
+												{badge}
+											</Badge>
+										))}
+									</span>
+								</h3>
+							</CardHeader>
+
+							{work.jobs.map((job, index) => (
+								<CardContent key={index} className='mt-2 mb-4 text-xs'>
+									<div className='flex flex-col md:flex-row md:items-center justify-between gap-x-2 text-sm'>
+										<h4 className='font-mono text-primary'>{job.title}</h4>
+										<time className='tabular-nums text-neutral-500'>
+											{job.start} - {job.end}
+										</time>
+									</div>
+									<ul className='mt-2'>
+										{job.description.map((item, index) => (
+											<li key={index} className='mb-1'>
+												• {item}
+											</li>
+										))}
+									</ul>
+								</CardContent>
+							))}
+						</Card>
+					))}
+				</Section>
+
+
+				<Section>
+					<h2 className='text-xl font-bold'>Skills</h2>
+					<div className='flex flex-wrap gap-1'>
+						{data.skills.map((skill) => (
+							<Badge key={skill}>{skill}</Badge>
+						))}
+					</div>
+				</Section>
+
+				<Section className='print-force-new-page scroll-mb-16'>
+					<h2 className='text-xl font-bold'>Projects</h2>
+					<div className='-mx-3 grid grid-cols-1 gap-3 print:grid-cols-3 print:gap-2 md:grid-cols-2 lg:grid-cols-3'>
+						{data.projects.map((project) => (
+							<ProjectCard
+								key={project.title}
+								title={project.title}
+								description={project.description}
+								tags={project.techStack}
+								link={'link' in project ? project.link.href : undefined}
+							/>
+						))}
+					</div>
+				</Section>
+			</section>
+
+			<CommandMenu
+				links={[
+					{
+						url: data.personalWebsiteUrl.url,
+						title: data.personalWebsiteUrl.name
+					},
+					{
+						url: `mailto:${data.contact.email.at}`,
+						title: data.contact.email.name
+					},
+					{
+						url: `tel:${data.contact.tel.phoneNumber}`,
+						title: data.contact.tel.name
+					},
+					...data.contact.social.map((socialMediaLink) => ({
+						url: socialMediaLink.url,
+						title: socialMediaLink.name
+					})),
+					{
+						url: data.contact.link.url,
+						title: data.contact.link.name
+					}
+				]}
+			/>
+		</main>
+	)
+}
---
+++ src/data/resume-data.tsx
@@ -0,0 +1,220 @@
+import { GitHubIcon, LinkedInIcon, XIcon } from '@/components/icons'
+
+export const RESUME_DATA = {
+	name: 'Govindh',
+	initials: 'GR',
+	location: 'Tamil Nadu,India',
+	locationLink: 'https://www.google.com/maps/place/tamilnadu',
+	about:
+		'Aspiring Full-Stack Developer | Design and Innovation Enthusiast',
+	summary: `Specialized in full-stack development with expertise in both frontend and backend technologies. Passionate about creating seamless,
+	 user-focused solutions that integrate cutting-edge technology and intuitive design. I strive to deliver impactful applications that make a difference,
+	  emphasizing social impact and innovation. Dedicated to leveraging advanced tools and frameworks to enhance web application functionality, scalability, 
+	  and user experience, ensuring robust and efficient solutions for every project.`,
+	avatar: '/assets/avatar.webp',
+	personalWebsiteUrl: {
+		name: 'Portfolio',
+		url: ''
+	},
+	contact: {
+		link: {
+			name: 'Other Links',
+			url: ''
+		},
+		email: {
+			name: 'Email',
+			at: 'grajbits@gmail.com'
+		},
+		tel: {
+			name: 'Phone',
+			phoneNumber: '+918531041822'
+		},
+		social: [
+			{
+				name: 'GitHub',
+				url: 'https://github.com/Gokulraj-max',
+				icon: GitHubIcon
+			},
+			{
+				name: 'LinkedIn',
+				url: 'https://www.linkedin.com/in/gokul-raj-089682249/',
+				icon: LinkedInIcon
+			},
+			{
+				name: 'X (Twitter)',
+				url: 'https://x.com/grajbits',
+				icon: XIcon
+			}
+		]
+	},
+	education: [
+
+		{
+			school: 'Sacred Heart College,Thiruppathur',
+			degree: "Cyber Security",
+			start: 'June,2024',
+			end: 'present',
+			description: [
+			"Building a strong foundation in cybersecurity principles, including network security, cryptography, and ethical hacking.",
+    "Learning the fundamentals of securing systems, identifying vulnerabilities, and preventing cyber threats.",
+    "Understanding various types of malware, phishing attacks, and their mitigation strategies.",
+    "Gaining proficiency in security tools like firewalls, antivirus software, and intrusion detection systems.",
+    "Exploring the basics of data protection, secure coding practices, and compliance with cybersecurity standards."
+			]
+		},
+
+		{
+			school: 'Sacred Heart College,Thiruppathur',
+			degree: "Master of Computer Applications",
+			start: 'June,2023',
+			end: 'present',
+			description: [
+			"Pursuing comprehensive studies in Frontend Development, .NET and C#, Java, Python, and Android Development.",
+    "Gained hands-on experience in software testing methodologies and best practices.",
+    "Developed and deployed applications using modern frameworks and programming languages.",
+    "Enhanced problem-solving skills by working on diverse projects involving mobile and web development.",
+			]
+		},
+
+		{
+			school: 'Don Bosco College,Yelagiri Hills',
+			degree: "Bachelor of Computer Application",
+			start: 'June, 2020',
+			end: 'April, 2023',
+			description: [
+				"Completed with 78%",
+				"Studied foundational and advanced concepts in programming, including C, C++, Java, and Python.",
+		"Developed skills in Database Management Systems (DBMS) for efficient data handling and storage solutions.",
+		"Explored Android Development, focusing on creating user-friendly mobile applications.",
+		"Engaged in academic projects that enhanced problem-solving and critical-thinking abilities in software development.",
+			]
+		},
+		{
+			school: 'Govt. Hr Sec School',
+			degree: "HSC",
+			start: 'June, 2019',
+			end: 'April, 2020',
+			description: [
+				
+		'Completed with 55%',
+			]
+		},
+		{
+			school: 'Govt. Hr Sec School',
+			degree: "SSLC",
+			start: 'June, 2017',
+			end: 'April, 2018',
+			description: [
+				"Completed with 80%",
+				
+			]
+		},
+		
+
+	],
+	work: [
+		{
+			company: 'Mobile Application Development',
+		//	link: 'https://github.com/thefrontendlab',
+			badges: ['Course'],
+			jobs: [
+				{
+					title: 'Sacred Heart College,Thiruppathur',
+					start: 'July, 2023',
+					end: 'December, 2023',
+					description: [
+						"Researched and implemented modern frontend technologies and tools to enhance application performance and user experience.",
+    "Developed and optimized mobile app interfaces to ensure responsive and intuitive designs.",
+    "Collaborated with cross-functional teams to integrate backend APIs with seamless frontend functionality."
+  
+					]
+				},
+	
+			]
+		},
+
+		{
+			company: 'Technology for Data Science',
+		//	link: 'https://github.com/thefrontendlab',
+			badges: ['Course'],
+			jobs: [
+				{
+					title: 'Sacred Heart College,Thiruppathur',
+					start: 'January, 2024',
+					end: 'April, 2024',
+					description: [
+						"Explored and utilized modern technologies and tools essential for data science applications.",
+        "Developed and implemented algorithms to analyze and interpret complex datasets.",
+        "Worked on real-world projects to apply machine learning techniques and data visualization strategies.",
+        "Collaborated on designing scalable solutions for processing and managing large data volumes."
+  
+					]
+				},
+	
+			]
+		},
+	
+		
+		
+	],
+	skills: [
+		'Android Studio',
+		'c#',
+		'CSS',
+		'GNU/Linux',
+		'HTML',
+		'Java(core)',
+		'JavaScript',
+		'Python',
+		'PHP',
+		'React.js',
+		'RESTful APIs',
+		'SQL (MySQL, PostgreSQL)',
+	],
+	projects: [
+		{
+			title: 'Resume Builder using HTML, CSS, JavaScript, and PHP',
+			techStack: [
+				'HTML','CSS','JavaScript','PHP','MySQL'
+			],
+			description:
+				'A simple yet powerful resume builder application built with HTML, CSS, JavaScript, and PHP. ',
+			link: {
+				label: '',
+				href: ''
+			}
+		},
+		{
+			title: 'Soul of Tune Mobile Application',
+			techStack: ['XML','Java','Firebase Database'],
+			description: 'A mobile app that connects students with skilled tutors to learn musical instruments and purchase quality instruments. ',
+			link: {
+				label: '',
+				href: ''
+			}
+		},
+		{
+			title: 'RR Bowl Mobile Application',
+			techStack: ['XML','Java','Firebase Database'],
+			description:
+				'RR BowlDeveloped a mobile app for ordering biryani with delivery and integrated payment options, ensuring a smooth and user-friendly experience.',
+			link: {
+				label: '',
+				href: ''
+			}
+		},
+		{
+			title: 'Notepad Editor Using C#',
+			techStack: [
+			'c#','MySQL',
+			],
+			description:
+			"Created a system-based Notepad application providing text editing and file management features.Implemented core functionalities to enhance user experience in managing plain text files.",
+		link: {
+				label: '',
+				href: ''
+			}
+		},
+
+	]
+}
